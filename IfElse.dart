a8d731e5244e5be7ed4c78438473e60566339315
TrollKiddi07 <67582814+TrollKiddi07@users.noreply.github.com>
2020-07-07 13:57:29 +0200

    Create IfElse.dart

---
+++ IfElse.dart
@@ -0,0 +1,16 @@
+main(){
+ int zahlengrenze=5;
+   
+   if(zahlengrenze > 10){//If Zweig
+     print("Die Zahlengrenze ist höher als 10");
+       
+ }
+ else if(zahlengrenze < 4) {
+    print("false");
+ }
+ else {
+   print("right");
+ }
+
+
+}
